9be82d1837bea55e22fc263c82e8ed8d7c4328f9
Ayushmaan Ratan <ayushmaanratan@gmail.com>
2026-02-13 00:22:23 +0530

    Update Cloudinary constants for configuration

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const CLOUDINARY_CLOUD_NAME = "dfhjvrhmb";
-export const CLOUDINARY_UPLOAD_PRESET = "images";
+export const CLOUDINARY_CLOUD_NAME = "YOUR CLOUDINARY NAME";
+export const CLOUDINARY_UPLOAD_PRESET = "YOUR PRESET NAME";
